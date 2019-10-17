2133cc6446805f8199010c2feb624e91e41fe2c0
Ivan Sedakov <lazofl24@gmail.com>
2019-10-17 22:18:40 +0200

    Updates

---
+++ precache-manifest.31e5abc3355089db62a806cf3affc302.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563cf5e06ae0656644689edb1d4cd7bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c09d6afd4a40398a1a8",
+    "url": "/static/css/3.3f4f9078.chunk.css"
+  },
+  {
+    "revision": "7795dbbb350ccb180889",
+    "url": "/static/css/main.df042fa1.chunk.css"
+  },
+  {
+    "revision": "e378450920f41f1fe750",
+    "url": "/static/js/2.5bd95512.chunk.js"
+  },
+  {
+    "revision": "1c09d6afd4a40398a1a8",
+    "url": "/static/js/3.bc5e5111.chunk.js"
+  },
+  {
+    "revision": "7795dbbb350ccb180889",
+    "url": "/static/js/main.0d6309c8.chunk.js"
+  },
+  {
+    "revision": "1f3604683709ae16052f",
+    "url": "/static/js/runtime-main.f39cdd62.js"
+  },
+  {
+    "revision": "2b8988d52fc0940e7ee7a213767b96c5",
+    "url": "/static/media/NoNameAvatar.2b8988d5.png"
+  },
+  {
+    "revision": "d7f4e79c454466a66c9e1fff1bea2cb6",
+    "url": "/static/media/mainPandaLogo.d7f4e79c.ico"
+  },
+  {
+    "revision": "2b52ebc2d2a5afb477c58dec702947ed",
+    "url": "/static/media/pandaAvatarForCommunites.2b52ebc2.jfif"
+  },
+  {
+    "revision": "86f81b053c824bd4e03d01c5e2efe2f7",
+    "url": "/static/media/pandaList.86f81b05.png"
+  },
+  {
+    "revision": "925f8148414ae23d908479d6a14c2b5e",
+    "url": "/static/media/pandaSearch.925f8148.png"
+  },
+  {
+    "revision": "4987b47d21622459955f95c652116c91",
+    "url": "/static/media/panda_loading.4987b47d.gif"
+  }
+]);
